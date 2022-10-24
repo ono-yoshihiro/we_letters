4618a58f2deb0c3fa86815a279bae08a02e83b11
EC2 Default User <ec2-user@ip-172-31-11-44.ap-northeast-1.compute.internal>
2022-10-24 12:30:02 +0000

    [Add]Bootstrap install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :sections,skip: [:passwords], controllers: {
+    registrations: "section/registrations",
+    sessions: 'section/sessions'
+  }
+
+  devise_for :admin,skip: [:passwords], controllers: {
+    registrations: "admin/registrations",
+    sessions: 'admin/sessions'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
